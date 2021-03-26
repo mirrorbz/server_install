58cd38d33b3077c2f6f9b13b9c567a5bef1a3590
mirrorbz <mirrorbz@gmail.com>
2021-03-26 22:34:17 +0900

    clean ファイル連番調整

--- common/11_1_php74_mysql.pl
+++ common/11_1_php74_mysql.pl
@@ -4,6 +4,7 @@
 use strict; use warnings;
 use FindBin;
 
+
 system("/usr/bin/yum install -y php74 php74-php php74-php-mysql php74-php-bcmath php74-php-ctype php74-php-json php74-php-mbstring php74-php-openssl php74-php-tokenizer php74-php-xml php74-php-gd php74-php-pdo php74-php-pear");
 system("/usr/bin/yum update  -y php74 php74-php php74-php-mysql php74-php-bcmath php74-php-ctype php74-php-json php74-php-mbstring php74-php-openssl php74-php-tokenizer php74-php-xml php74-php-gd php74-php-pdo php74-php-pear");
 
@@ -12,5 +13,8 @@ if (-e '/usr/bin/php' && -l '/usr/bin/php' ) {
 }
 system("/bin/ln -sf /usr/bin/php74 /usr/bin/php");
 
-my $php_ini_script = sprintf("%s/03_9_php_ini.pl", $FindBin::Bin);
+system("/usr/bin/yum install -y php74-php-mysqlnd");
+system("/usr/bin/yum update  -y php74-php-mysqlnd");
+
+my $php_ini_script = sprintf("%s/11_9_php_ini.pl", $FindBin::Bin);
 system($php_ini_script);
--- common/11_2_php74_laravel.pl
+++ common/11_2_php74_laravel.pl
@@ -4,7 +4,6 @@
 use strict; use warnings;
 use FindBin;
 
-
 system("/usr/bin/yum install -y php74 php74-php php74-php-mysql php74-php-bcmath php74-php-ctype php74-php-json php74-php-mbstring php74-php-openssl php74-php-tokenizer php74-php-xml php74-php-gd php74-php-pdo php74-php-pear");
 system("/usr/bin/yum update  -y php74 php74-php php74-php-mysql php74-php-bcmath php74-php-ctype php74-php-json php74-php-mbstring php74-php-openssl php74-php-tokenizer php74-php-xml php74-php-gd php74-php-pdo php74-php-pear");
 
@@ -13,8 +12,5 @@ if (-e '/usr/bin/php' && -l '/usr/bin/php' ) {
 }
 system("/bin/ln -sf /usr/bin/php74 /usr/bin/php");
 
-system("/usr/bin/yum install -y php74-php-mysqlnd");
-system("/usr/bin/yum update  -y php74-php-mysqlnd");
-
-my $php_ini_script = sprintf("%s/03_9_php_ini.pl", $FindBin::Bin);
+my $php_ini_script = sprintf("%s/11_9_php_ini.pl", $FindBin::Bin);
 system($php_ini_script);
